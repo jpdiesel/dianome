48fed96a6abe69bca31831a3f402302d7e91c494
João Pedro Diesel <jpedro.diesel@gmail.com>
2023-11-23 19:09:11 -0300

    (feat): add model layer

---
+++ dianome/src/main/java/com/dianome/dianome/model/EmployeeModel.java
@@ -0,0 +1,57 @@
+package com.dianome.dianome.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+
+public class EmployeeModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String nome;
+  private Integer cpf;
+
+  @Column(name = "capacidade_veiculo_kg")
+  private Integer capacidade;
+
+  public EmployeeModel(String nome, Integer cpf, Integer capacidade) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.capacidade = capacidade;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public Integer getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(Integer cpf) {
+    this.cpf = cpf;
+  }
+
+  public Integer getCapacidade() {
+    return capacidade;
+  }
+
+  public void setCapacidade(Integer capacidade) {
+    this.capacidade = capacidade;
+  }
+}
