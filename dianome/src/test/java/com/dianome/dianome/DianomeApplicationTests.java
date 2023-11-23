b779bf20683a3decafc6f60b23ffcd36a178decc
João Pedro Diesel <jpedro.diesel@gmail.com>
2023-11-23 18:05:23 -0300

    starting project

---
+++ dianome/src/test/java/com/dianome/dianome/DianomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dianome.dianome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DianomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
