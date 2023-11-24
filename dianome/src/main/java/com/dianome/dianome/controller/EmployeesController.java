4617d0f14cb1a719dc9babccdf4f83ce5e9fd118
João Pedro Diesel <jpedro.diesel@gmail.com>
2023-11-24 13:30:15 -0300

    (feat): add PUT endpoint and functions

--- dianome/src/main/java/com/dianome/dianome/controller/EmployeesController.java
+++ dianome/src/main/java/com/dianome/dianome/controller/EmployeesController.java
@@ -1,12 +1,15 @@
 package com.dianome.dianome.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dianome.dianome.model.EmployeeModel;
@@ -33,5 +36,15 @@ public class EmployeesController {
       employeeRepository.deleteById(id);
       return true;
     } return false;
+  }
+
+  @PutMapping(value = "/empregados/{id}")
+  public EmployeeModel editEmployee(@PathVariable("id") Integer id, @RequestBody Map<String, String> body) {
+    EmployeeModel current = employeeRepository.findById(id).get();
+    current.setCapacidade(Integer.parseInt(body.get("capacidade")));
+    current.setNome(body.get("nome"));
+    current.setCpf(body.get("cpf"));
+    employeeRepository.save(current);
+    return current;
   }
 }
